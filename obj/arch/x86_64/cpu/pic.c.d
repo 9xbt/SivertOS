ea1321dd73e3871b78ea2ed73c91311d38f8c627
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-05-03 16:51:12 +0200

    Delete obj directory

--- obj/arch/x86_64/cpu/pic.c.d
+++
@@ -1,5 +0,0 @@
-obj/arch/x86_64/cpu/pic.c.o: src/arch/x86_64/cpu/pic.c \
- src/arch/x86_64/cpu/pic.h src/types.h src/arch/x86_64/io.h
-src/arch/x86_64/cpu/pic.h:
-src/types.h:
-src/arch/x86_64/io.h:
--- obj/tools/logger.c.d
+++
@@ -1,7 +0,0 @@
-obj/tools/logger.c.o: src/tools/logger.c src/tools/logger.h src/types.h \
- src/kernel/kernel.h src/arch/x86_64/cpu/serial.h src/flanterm/flanterm.h
-src/tools/logger.h:
-src/types.h:
-src/kernel/kernel.h:
-src/arch/x86_64/cpu/serial.h:
-src/flanterm/flanterm.h:
